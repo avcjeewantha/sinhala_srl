654a889b4f6e99cea92a8d2b39f7ae33d0773d25
sandunK <gmgsk.16@cse.mrt.ac.lk>
2020-03-03 00:09:57 +0530

    Implemented english annotator

--- src/main/java/sinhala/semantic/annotator/Annotator.java
+++ src/main/java/sinhala/semantic/annotator/Annotator.java
@@ -1,7 +1,14 @@
 package sinhala.semantic.annotator;
 
-public class Annotator {
-    public static void main(String args[]){
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Annotator {
+    public static void main(String args[]) {
+        final Logger logger = LoggerFactory.getLogger(Annotator.class);
+        logger.error("Project is starting...");
+        EnglishPipeline pipeline = new EnglishPipeline();
+        Sentence parsedSL = pipeline.parse("I am going home");
+        logger.info(parsedSL.toConllU());
     }
 }
