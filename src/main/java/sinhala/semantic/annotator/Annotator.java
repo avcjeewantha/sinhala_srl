200dd820466354613488750422a3a244cc6fa0a3
sandunK <gmgsk.16@cse.mrt.ac.lk>
2020-03-05 18:34:06 +0530

    Add recursive implementation for the main program

--- src/main/java/sinhala/semantic/annotator/Annotator.java
+++ src/main/java/sinhala/semantic/annotator/Annotator.java
@@ -2,13 +2,28 @@ package sinhala.semantic.annotator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.Scanner;
 
 public class Annotator {
     public static void main(String args[]) {
         final Logger logger = LoggerFactory.getLogger(Annotator.class);
         logger.error("Project is starting...");
         EnglishPipeline pipeline = new EnglishPipeline();
-        Sentence parsedSL = pipeline.parse("I am going home");
-        logger.info(parsedSL.toConllU());
+        // Initiate scanner
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Enter a string");
+            String userInput = sc.nextLine();
+            if (userInput.equals("e")){
+                break;
+            }else{
+                Sentence parsedSL = pipeline.parse(userInput);
+                logger.info(parsedSL.toConllU());
+            }
+
+        }
+
+
     }
 }
